4e8526f64fcd414c6315bf598c785379a74c9225
dashiwa <moscovie.com@gmail.com>
2021-01-07 18:40:46 +0300

    R-03 RNCGC stage first

--- php/src/ThirdProcessRule.php
+++ php/src/ThirdProcessRule.php
@@ -9,24 +9,31 @@ class ThirdProcessRule
     public function thirdProcessRule($item)
     {
         if ($item->sell_in < 0) {
+
             if ($item->name != 'Aged Brie') {
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+                return $this->notBackstagePassesItem($item);
             }
 
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+            }
 
         }
     }
+
+    public function notBackstagePassesItem($item)
+    {
+        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->quality > 0) {
+                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+        } else {
+            $item->quality = $item->quality - $item->quality;
+        }
+
+    }
+
 
 }
