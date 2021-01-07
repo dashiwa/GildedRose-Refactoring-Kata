fff28aa5618fdb618a47a944dc8e503f5039b734
dashiwa <moscovie.com@gmail.com>
2021-01-07 18:35:06 +0300

    R-02  Replace Nested Conditional with Guard Clauses for FirstProcessRule block

--- php/src/FirstProcessRule.php
+++ php/src/FirstProcessRule.php
@@ -9,26 +9,33 @@ class FirstProcessRule
     public function firstProcessRule($item)
     {
         if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = $item->quality - 1;
+            return $this->qualityAboveZero($item);
+        }
+
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + 1;
+            if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->sell_in < 11) {
+                    if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
+                }
+                if ($item->sell_in < 6) {
+                    if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
                 }
             }
-        } else {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                    if ($item->sell_in < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
+        }
+
+
+    }
+
+    public function qualityAboveZero($item)
+    {
+        if ($item->quality > 0) {
+            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                $item->quality = $item->quality - 1;
             }
         }
     }
