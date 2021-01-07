8f3c86519ff9fd098706bd95fcbc36e0730c71bb
dashiwa <moscovie.com@gmail.com>
2021-01-07 18:44:43 +0300

    R-04 RNCGC stage second

--- php/src/ThirdProcessRule.php
+++ php/src/ThirdProcessRule.php
@@ -24,15 +24,20 @@ class ThirdProcessRule
     public function notBackstagePassesItem($item)
     {
         if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = $item->quality - 1;
-                }
-            }
-        } else {
-            $item->quality = $item->quality - $item->quality;
+            return $this->itemQualityAboveZero($item);
         }
 
+        $item->quality = $item->quality - $item->quality;
+
+    }
+
+    public function itemQualityAboveZero($item)
+    {
+        if ($item->quality > 0) {
+            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                $item->quality = $item->quality - 1;
+            }
+        }
     }
 
 
