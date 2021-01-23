bd2e3c93069a01babced6a0f5c265ae13cd19c05
dashiwa <moscovie.com@gmail.com>
2021-01-23 13:57:32 +0300

    R-10 Optimized loop

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -12,30 +12,26 @@ final class GildedRose
      */
     private $items;
 
+    private $firstProcessRule;
+    private $secondProcessRule;
+    private $thirdProcessRule;
+
     public function __construct(array $items)
     {
+        $itemQuality = new ItemQuality();
         $this->items = $items;
+        $this->firstProcessRule = new FirstProcessRule($itemQuality);
+        $this->secondProcessRule = new SecondProcessRule();
+        $this->thirdProcessRule = new ThirdProcessRule($itemQuality);
+
     }
 
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-
-            $itemQuality = new ItemQuality();
-            $firstProcessRule = new FirstProcessRule($itemQuality);
-
-            $firstProcessRule->firstProcessRule($item);
-
-
-            $secondProcessRule = new SecondProcessRule();
-
-            $secondProcessRule->secondProcessRule($item);
-
-
-            $thirdProcessRule = new ThirdProcessRule($itemQuality);
-
-            $thirdProcessRule->thirdProcessRule($item);
-
+            $this->firstProcessRule->firstProcessRule($item);
+            $this->secondProcessRule->secondProcessRule($item);
+            $this->thirdProcessRule->thirdProcessRule($item);
         }
     }
 }
---
+++ php/src/SellInFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace GildedRose;
+
+
+class SellInFactory
+{
+
+}
